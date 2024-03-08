2696e0f67ad4d881355daebbec4466e1904b5328
ahmd-nabil <ahm3dnabil99@gmail.com>
2024-03-08 20:30:49 +0200

    initial commit

---
+++ src/test/java/nabil/awsdynamodbexample/AwsDynamodbExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package nabil.awsdynamodbexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDynamodbExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
